ea44a83eb99eb06dddb1ab79cbb424f32eed5b29
Glinfen <gilinfen@gmail.com>
2023-12-20 14:25:49 +0800

    feat: 🎸 Facebook 爬取粉丝

---
+++ python/Facebook/vermicelli.py
@@ -0,0 +1,135 @@
+import time
+import re
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+import json
+
+# 垃圾文件地址
+# /private/var/folders/36/csqnmwj93c7gx0003_19fv1m0000gn
+
+# 打开并读取JSON文件
+# with open('./settings.json', 'r') as file:
+#     data = json.load(file)
+
+# 打印读取的数据
+# print(f"chromedriver：{data['chromedriver']}")
+# chromedriver_path = data["chromedriver"]
+chromedriver_path = '/Users/Fox/Code/Rust/Arachnoid/python/chromedriver'
+utl = "https://www.facebook.com/jl.ukay.shoes.supplier"
+
+# 指定 chromedriver 的路径
+service = Service(chromedriver_path)
+driver = webdriver.Chrome(service=service)
+driver.get(utl)
+
+# 找到用户名和密码输入框
+username_input = driver.find_element("xpath",
+                                     '//*[@id="login_popup_cta_form"]/div/div[3]/div/label/div/div/input')  # 替换为用户名输入框的实际 id 或其他定位器
+password_input = driver.find_element("xpath",
+                                     '//*[@id="login_popup_cta_form"]/div/div[4]/div/label/div/div[1]/input')  # 替换为密码输入框的实际 id 或其他定位器
+# 输入用户名和密码
+username_input.send_keys("glinfen@gmail.com")  # 替换为你的用户名
+
+password_input.send_keys("951299034.glf")  # 替换为你的密码
+driver.find_element("xpath", '//*[@id="login_popup_cta_form"]/div/div[5]/div/div').click()
+
+
+def web_driver_wait_button_click(utl, timeout=10):
+    wait = WebDriverWait(driver, timeout)
+    button = wait.until(EC.element_to_be_clickable((By.XPATH, utl)))
+    button.click()
+    return button
+
+
+driver.execute_script(f"window.location.href = '{utl}/followers';")
+web_driver_wait_button_click('/html/body/div[5]')
+
+
+# 获取用户 ID
+def extract_facebook_id_or_username(url):
+    # 正则表达式用于匹配两种不同的 Facebook URL
+    pattern = r'facebook\.com/(profile\.php\?id=(\d+)|([^/?]+))'
+    match = re.search(pattern, url)
+
+    if match:
+        # 如果匹配到 'profile.php?id=...' 格式，返回数字 ID
+        if match.group(2):
+            return match.group(2)
+        # 否则返回用户名
+        return match.group(3)
+    else:
+        return None  # 如果不匹配，返回 None
+
+
+def wait_for_child_element(parent, child_locator, timeout=10):
+    try:
+        WebDriverWait(parent, timeout).until(
+            lambda x: len(parent.find_elements(*child_locator)) > 0
+        )
+        return parent.find_element(*child_locator)
+    except TimeoutException:
+        return None
+
+
+# 设置显式等待
+wait = WebDriverWait(driver, 60)
+trigger_element_xpath = "/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div/div/div/div/div/div[3]"
+parent_div = driver.find_element("xpath",
+                                 "/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div/div/div/div/div/div[3]")
+
+processed_ids = set()  # 用来跟踪已处理的元素ID
+
+
+def get_elem_info():
+    return driver.find_elements("xpath",
+                                "/html/body/div[1]/div/div[1]/div/div[3]/div/div/div[1]/div[1]/div/div/div[4]/div/div/div/div/div/div/div/div/div[3]/div")  # 替换为列表项的 XPath
+
+def wait_until_condition_met(timeout=60):
+    start_time = time.time()
+    previous_len = 0
+
+    while True:
+        elements_list = get_elem_info()
+        current_len = len(elements_list)
+
+        if current_len >= previous_len:
+            print(current_len)
+            return True  # 条件满足，返回 True
+
+        if time.time() - start_time > timeout:
+            return False  # 超时，返回 False
+
+        previous_len = current_len
+        time.sleep(1)
+
+is_falg = True
+while is_falg:
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    if not wait_until_condition_met():
+        # 如果条件没有满足（例如超时），则退出外层循环
+        break
+    # # 这里是条件满足时继续执行的代码
+    time.sleep(10)
+    for element in get_elem_info():
+        element_href = element.find_element("xpath", "./div[1]/a").get_attribute('href')
+        element_id = extract_facebook_id_or_username(element_href)
+        user_data = {
+            "name": element.find_element("xpath", "./div[2]/div[1]").text,
+            "icon": element.find_element("xpath", "./div[1]/a/img").get_attribute('src'),
+            "id": element_id,
+            "home_page": element_href,
+            "description": element.find_element("xpath", "./div[2]/div[2]").text,
+            "chat": "https://www.facebook.com/messages/t/" + element_id
+        }
+        if element_id not in processed_ids:
+            processed_ids.add(element_id)
+            # 处理 user_data...
+            print(user_data)
+
+
+driver.quit()
--- python/sele.py
+++ python/sele.py
@@ -1,21 +1,17 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-import json
-
-# 打开并读取JSON文件
-# with open('./settings.json', 'r') as file:
-#     data = json.load(file)
-
-# 打印读取的数据
-# print(f"chromedriver：{data['chromedriver']}")
-# chromedriver_path = data["chromedriver"]
 chromedriver_path = '/Users/Fox/Code/Rust/Arachnoid/python/chromedriver'
 
 # 指定 chromedriver 的路径
 service = Service(chromedriver_path)
+# driver = webdriver.Chrome()
 driver = webdriver.Chrome(service=service)
 driver.get('http://www.baidu.com')
-driver.implicitly_wait(10)
+# driver.implicitly_wait(10)
+time.sleep(5)
 driver.find_element('xpath', '//*[@id="kw"]').send_keys('日期')
 driver.find_element('id', 'su').click()
+time.sleep(5)
 driver.quit()
