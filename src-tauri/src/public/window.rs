cf2e217791a8b1d41c00937a35b02864509ba7c9
Glinfen <gilinfen@gmail.com>
2024-01-03 02:23:21 +0800

    perf: ⚡️ 测试第一版

--- src-tauri/src/public/window.rs
+++ src-tauri/src/public/window.rs
@@ -1,9 +1,8 @@
 use tauri::{AppHandle, Manager};
 
 pub fn app_ready(app: AppHandle) {
-    // app.windows().clear()
     // 关闭加载窗口
-    if let Some(loading_window) = app.get_window("arachnoid") {
+    if let Some(loading_window) = app.get_window("activate") {
         loading_window.close().unwrap();
     }
 
